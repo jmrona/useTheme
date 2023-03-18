1fc2837a55a063c6994ab898b6b3c2e80fb3691b
jmrona <jm_rona@hotmail.com>
2023-03-18 20:39:31 +0000

    Adding context

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useEffect, useState } from "react";
+import { config } from "../../config";
+
+export const ThemeContext = createContext("");
+
+export const ThemeProvider = ({ children }) => {
+  const themeDefault = config.websiteId == 1 ? "forth" : "edge";
+  const variantDefault = window.matchMedia("(prefers-color-scheme: dark)")?.matches ? "dark" : "light";
+  const [themeStore, variantStore] = localStorage.getItem("theme")?.split("-") || [undefined, undefined];
+
+  const [theme, setTheme] = useState(themeStore || themeDefault);
+  const [variant, setVariant] = useState(variantStore || variantDefault);
+
+  useEffect(() => {
+    if (theme && variant) {
+      localStorage.setItem("theme", `${theme}-${variant}`);
+      document.documentElement.setAttribute("data-theme", theme);
+      document.documentElement.setAttribute("data-variant", variant);
+    }
+
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", handleListener);
+
+    return () => window.removeEventListener("change", handleListener);
+  }, [theme, variant]);
+
+  const handleListener = (e) => {
+    const variantListener = e.matches ? "dark" : "light";
+    setVariant(variantListener);
+  };
+
+  const toggleVariant = () => {
+    const newVariant = variant == "dark" ? "light" : "dark";
+    setVariant(newVariant);
+
+    document.documentElement.setAttribute("data-variant", newVariant);
+  };
+
+  const toggleTheme = () => {
+    const newTheme = theme == "forth" ? "edge" : "forth";
+    setTheme(newTheme);
+
+    document.documentElement.setAttribute("data-theme", newTheme);
+  };
+
+  return <ThemeContext.Provider value={{ theme, variant, toggleTheme, toggleVariant }}>{children}</ThemeContext.Provider>;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ThemeProvider>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
